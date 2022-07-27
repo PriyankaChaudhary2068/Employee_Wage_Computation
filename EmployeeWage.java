7e5a396987a696a689c9b330991180435a865e07
chutki12 <priyankachaudhary2068>
2022-07-27 18:00:46 +0530

    ADDED : This Code For To Check Employee Is Present Or Absent

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,10 +2,22 @@ package com.bridgelabz.employeeWage;
 
 public class EmployeeWage {
 
-	public static void main(String[] args) {
-	
-		System.out.println("Welcome To Employee Wage Computation Program");
+    static final int IS_PRESENT = 1;
 
-	}
-
+    public static void main(String[] args) {
+    	
+        System.out.println("Welcome to Employee Wage Computation Program");
+        double empCheck = Math.floor(Math.random() * 10) % 2;
+        if (empCheck == IS_PRESENT)
+            System.out.println("Employee is Present");
+        else
+            System.out.println("Employee is Absent");
+        
+    }
 }
+
+
+
+	
+
+
