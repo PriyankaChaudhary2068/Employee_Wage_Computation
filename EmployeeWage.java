77451148fc5d149b98940397b5ca2320890382f6
chutki12 <priyankachaudhary2068>
2022-07-28 12:01:19 +0530

    REFACTORED : This Code For To Compute Employee Wage Using Class Method

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,30 +1,37 @@
 package com.bridgelabz.employeeWage;
 
+
 public class EmployeeWage {
 
-            static final int IS_FULL_TIME = 1;
-		    static final int IS_PART_TIME = 2;
-		    static final int EMP_WAGE_PER_HOUR = 20;
-		    static final int EMP_FULL_DAY_HOUR = 8;
-		    static final int EMP_HALF_DAY_HOUR = 4;
-		    static final int TOTAL_WORKING_DAYS = 20;
-		    static final int TOTAL_WORKING_HOURS = 100;
+      static final int IS_FULL_TIME = 1;
+      static final int IS_PART_TIME = 2;
+      static final int IS_ABSENT = 0;
+      static final int EMP_WAGE_PER_HOUR = 20;
+      static final int EMP_FULL_DAY_HOUR = 8;
+      static final int EMP_HALF_DAY_HOUR = 4;
+      static final int TOTAL_WORKING_DAYS = 20;
+      static final int TOTAL_WORKING_HOURS = 100;
+		    
+		   int workingHour = 0;
+		   int countFullDaysWorking = 0;
+		   int countHalfDayWorking = 0;
+		   int countAbsentDays = 0;
+		   
+		   public static void main(String[] args) {
 
-		    public static void main(String[] args) {
-		        System.out.println("Welcome to Employee Wage Computation Program");
-		        int employeeWage;
-		        int workingHour = 0;
-		        int countFullDaysWorking = 0;
-		        int countHalfDayWorking = 0;
-		        int countAbsentDays = 0;
-		        int totalWorkingDays = 0;
-		        
+		       System.out.println("Welcome to Employee Wage Computation Project");
+		       EmployeeWage  refVar = new EmployeeWage ();
+		       refVar.empAttendance();
+		       refVar.empWageCalculation();
+		    }
+
+		    void empAttendance() {
+
 		        for (int day = 0; day < TOTAL_WORKING_DAYS; day++) {
 		            if (workingHour > TOTAL_WORKING_HOURS) {
 		                workingHour = TOTAL_WORKING_HOURS;
 		                break;
 		            }
-		            
 		            double empCheck = Math.floor(Math.random() * 10) % 3;
 		            switch ((int) empCheck) {
 		                case IS_FULL_TIME:
@@ -37,13 +44,20 @@ public class EmployeeWage {
 		                    countHalfDayWorking++;
 		                    break;
 		                    
-		                default:
+		                case IS_ABSENT:
 		                    countAbsentDays++;
 		            }
 		        }
-		        
-		        if (countAbsentDays == 20)
-		            System.out.println("Employee is absent for month");
+		    }
+
+		    void empWageCalculation() {
+
+		        int employeeWage;
+		        int totalWorkingDays;
+
+		        if (countAbsentDays == TOTAL_WORKING_DAYS)
+		           System.out.println("Employee is absent for month");
+
 		        employeeWage = workingHour * EMP_WAGE_PER_HOUR;
 		        totalWorkingDays = countFullDaysWorking + countHalfDayWorking;
 		        System.out.println("Employee is Absent for : " + countAbsentDays + " days.");
@@ -52,8 +66,10 @@ public class EmployeeWage {
 		        System.out.println("Employee total working hour is : " + workingHour + " hrs");
 		        System.out.println("Employee is present for " + totalWorkingDays + " days in a month and wage is : " + employeeWage);
 		    }
-
 		}
 
 
-		    
+
+		        
+		        
+		        		    
